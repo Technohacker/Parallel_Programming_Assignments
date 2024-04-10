fc6847d0bc1b0c6664f9c28041ca09915c26b67a
Technohacker <technohacker1995@gmail.com>
2024-04-10 09:32:59 +0530

    Add verifier

---
+++ Assignment3/verifier/verifier.cpp
@@ -0,0 +1,84 @@
+// Reads a file of integers and verifies that the integers are in sorted order,
+// while also ensuring that the frequency of each integer is the same as the original file.
+
+#include <array>
+#include <cstddef>
+#include <iostream>
+#include <fstream>
+
+#include "../common.h"
+
+typedef std::array<size_t, ((size_t) 1) << ELEMENT_SIZE * 8> freq_t;
+
+bool check_and_decrement(freq_t* freq, element_t elem) {
+    if ((*freq)[elem] == 0) {
+        return false;
+    }
+
+    (*freq)[elem]--;
+    return true;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <original file> <sorted file>" << std::endl;
+        return 1;
+    }
+
+    std::string original_file = argv[1];
+    std::string sorted_file = argv[2];
+
+    std::ifstream original(original_file, std::ios::binary);
+    if (!original.is_open()) {
+        std::cerr << "Failed to open original file." << std::endl;
+        return 1;
+    }
+
+    std::ifstream sorted(sorted_file, std::ios::binary);
+    if (!sorted.is_open()) {
+        std::cerr << "Failed to open sorted file." << std::endl;
+        return 1;
+    }
+
+    freq_t *histogram = new freq_t();
+
+    element_t elem, last_elem;
+    while (original.read(reinterpret_cast<char*>(&elem), ELEMENT_SIZE)) {
+        (*histogram)[elem]++;
+    }
+
+    bool first = true;
+    while (sorted.read(reinterpret_cast<char*>(&elem), ELEMENT_SIZE)) {
+        if (sorted.eof()) {
+            std::cerr << "Sorted file is shorter than the original file." << std::endl;
+            return 1;
+        }
+
+        if (first) {
+            first = false;
+        } else {
+            if (elem < last_elem) {
+                std::cerr << "Sorted file is not in sorted order. Element " << elem << " is less than " << last_elem << " at position " << sorted.tellg() << "." << std::endl;
+                return 1;
+            }
+        }
+        last_elem = elem;
+
+        if (!check_and_decrement(histogram, elem)) {
+            std::cerr << "Element " << elem << " is missing from the original file." << std::endl;
+            return 1;
+        }
+    }
+
+    for (size_t i = 0; i < histogram->size(); i++) {
+        if ((*histogram)[i] != 0) {
+            std::cerr << "Element " << i << " is missing from the sorted file." << std::endl;
+            return 1;
+        }
+    }
+
+    std::cout << "Files are identical." << std::endl;
+    delete histogram;
+
+    return 0;
+}
